e264ff5f05b42e0cb3a9cf1379d67de74dc89e31
Manuel Carballar <manuel.carballar@ST-A104-36RG8WP.domino.softonic.com>
2016-07-04 19:38:53 +0200

    Fix JavaHelloWorld.java

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -4,6 +4,6 @@ public class JavaHelloWorld
 	{
 		System.out.println("Java Hello World");
 		System.out.println("Hello again");
-		System.oul.println("Hello again per three time!");
+		System.out.println("Hello again per three time!");
 	}
 }
